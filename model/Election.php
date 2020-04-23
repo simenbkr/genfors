1ed838919f3ca23410b37ea1e18367d49d0d4c58
simenbkr <simen@haitech.org>
2020-04-23 23:24:49 +0200

    Fix proper handling of checking whether a user has voted in a general election on the backend.

--- model/Election.php
+++ model/Election.php
@@ -102,16 +102,11 @@ class Election
 
     public function registerVote(Alternative $alternative, User $user)
     {
-        $st = DB::getDB()->prepare('SELECT * FROM votes WHERE val = :val');
-        $st->execute(['val' => $this->generateElectionKey($user)]);
-
-        if($st->rowCount() > 0) {
-            return;
+        if(!$this->hasVoted($user)) {
+            $st = DB::getDB()->prepare('INSERT INTO votes(val) VALUES(:val)');
+            $st->execute(['val' => $this->generateElectionKey($user)]);
+            $alternative->incrementVotes();
         }
-
-        $st = DB::getDB()->prepare('INSERT INTO votes(val) VALUES(:val)');
-        $st->execute(['val' => $this->generateElectionKey($user)]);
-        $alternative->incrementVotes();
     }
 
     public static function allActive(): array
